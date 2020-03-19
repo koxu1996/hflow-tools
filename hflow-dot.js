c47a8069f9761cbd83d66cc614a0683135831d35
Bartosz Balis <balis@agh.edu.pl>
2020-03-19 22:12:17 +0000

    Add option to generate full workflow graph

--- hflow-dot.js
+++ hflow-dot.js
@@ -10,18 +10,20 @@ var fs       = require('fs'),
 var doc = "\
 hflow-dot: converts HyperFlow workflow.json to graphviz dot format\n\
 Usage:\n\
-  hflow-dot [-p] [--png] <workflow-json-file-path>\n\
+  hflow-dot [-p] [--full] [--png] <workflow-json-file-path>\n\
   hflow-dot -h|--help\n\
   \
 Options:\n\
   -h --help   Prints this\n\
   --png       Generate png\n\
+  --full      Generate full graph (with input and output nodes)\n\
   -p          Generates graph according to paritioning info";
 
 var opts = docopt(doc);
 
 var partitioning = opts['-p'];
 var png = opts['--png'];
+var full = opts['--full'];
 
 var file = opts['<workflow-json-file-path>'];
 
@@ -47,51 +49,92 @@ var sigToId = function(sig) {
   }
 }
 
-// get a graphlib representation of the workflow process graph
-var procg = hfgraph(wf).procGraph;
+if (full) { // generate full graph
+  processes.forEach(function(e, ix) {
+    var id = "t_" + ix;
+    var n = g.addNode(id);
+    n.set('label', e.name);
 
-// generate colormap
-var procNames = {}; 
-processes.forEach(function(proc, idx) {
-  procNames[proc.name]="1";
-});
-var procNamesArray = Object.keys(procNames).sort((a,b) => a.substring(1).localeCompare(b.substring(1)));
-procNamesArray.forEach((pn, idx) => procNames[pn]=idx); // map name to index
-
-let nColors = partitioning ? 2: procNamesArray.length;
-let cmap = nColors > 10 ? 'hsv': 'jet';
-if (nColors < 6) { cmap = 'autumn'; }
-let colors = colormap({
-    colormap: cmap,
-    nshades: nColors,
-    format: 'hex',
-    alpha: 1
-});
-
-// build the graphviz graph and save it
-procg.nodes().forEach(function(proc, idx) {
-  var n = g.addNode(proc);
-  var name = procg.node(proc);
-  let procId = Number(proc.split(':')[1])-1;
-  if (partitioning) {
-    n.set('label', "");
-    let parNum = processes[procId].partition-1;
-    n.set('color', colors[parNum]);
-  } else {
-    n.set('label', name);
-    n.set('color', colors[procNames[name]]);
-  }
-  // special storage node
-  if (processes[procId].type == "special") {
-    n.set('shape', 'cylinder');
-    n.set('width', 1.0);
-    n.set('height', 1.0);
-  }
-  n.set('style', 'filled');
-  procg.successors(proc).forEach(function(succ) {
-    g.addEdge(proc, succ);
+    // special storage node
+    if (processes[ix].type == "special") {
+      n.set('shape', 'cylinder');
+      n.set('width', 1.0);
+      n.set('height', 1.0);
+    }
+ 
+    e.ins.forEach(function(e){
+      var n = e.toString().split(":")[0];
+      g.addEdge("d_" + sigToId(n), id);
+    })
+    e.outs.forEach(function(e){
+      var n = e.toString().split(":")[0];
+      g.addEdge(id, "d_" + sigToId(n));
+    })
   });
-});
+
+  var sigNodes = {};
+
+  signals.forEach(function(e, ix) {
+    var n = g.addNode("d_" + ix);
+    n.set('label', e.name);
+    n.set('shape', "folder");
+    sigNodes[ix] = n;
+  });
+
+  wf.ins.forEach(function(e){
+    sigNodes[sigToId(e)].set('color', 'green');
+  });
+
+  wf.outs.forEach(function(e){
+    sigNodes[sigToId(e)].set('color', 'red');
+  });
+} else {
+  // get a graphlib representation of the workflow graph
+  var procg = full ? hfgraph(wf).fullGraph: hfgraph(wf).procGraph;
+
+  // generate colormap
+  var procNames = {}; 
+  processes.forEach(function(proc, idx) {
+    procNames[proc.name]="1";
+  });
+  var procNamesArray = Object.keys(procNames).sort((a,b) => a.substring(1).localeCompare(b.substring(1)));
+  procNamesArray.forEach((pn, idx) => procNames[pn]=idx); // map name to index
+
+  let nColors = partitioning ? 2: procNamesArray.length;
+  let cmap = nColors > 10 ? 'hsv': 'jet';
+  if (nColors < 6) { cmap = 'autumn'; }
+  let colors = colormap({
+      colormap: cmap,
+      nshades: nColors,
+      format: 'hex',
+      alpha: 1
+  });
+
+  // build the graphviz graph and save it
+  procg.nodes().forEach(function(proc, idx) {
+    var n = g.addNode(proc);
+    var name = procg.node(proc);
+    let procId = Number(proc.split(':')[1])-1;
+    if (partitioning) {
+      n.set('label', "");
+      let parNum = processes[procId].partition-1;
+      n.set('color', colors[parNum]);
+    } else {
+      n.set('label', name);
+      n.set('color', colors[procNames[name]]);
+    }
+    // special storage node
+    if (processes[procId].type == "special") {
+      n.set('shape', 'cylinder');
+      n.set('width', 1.0);
+      n.set('height', 1.0);
+    }
+    n.set('style', 'filled');
+    procg.successors(proc).forEach(function(succ) {
+      g.addEdge(proc, succ);
+    });
+  });
+}
 
 let basename = file.substring(0, file.lastIndexOf('.'));
 
