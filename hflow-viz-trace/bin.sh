4301c3c2d4b8629b4de24e0fed8e595d75a10a40
Bartosz Balis <balis@agh.edu.pl>
2020-06-11 21:29:51 +0000

    Remove virtualenvs (not necessary and problematic in containers)

--- hflow-viz-trace/bin.sh
+++ hflow-viz-trace/bin.sh
@@ -4,15 +4,17 @@ set -e
 PACKAGE="hflow-tools"
 NPM_ROOT=$(npm root -g)
 
-pushd . > /dev/null
-cd ${NPM_ROOT}/${PACKAGE}/hflow-viz-trace
+#pushd . > /dev/null
+#cd ${NPM_ROOT}/${PACKAGE}/hflow-viz-trace
 
-PYTHON="$(pipenv --venv)/bin/python"
-if [ ! -f $PYTHON ]; then
-  # fallback to windows path
-  PYTHON="$(pipenv --venv)/Scripts/python.exe"
-fi
+#PYTHON="$(pipenv --venv)/bin/python"
+#if [ ! -f $PYTHON ]; then
+#  # fallback to windows path
+#  PYTHON="$(pipenv --venv)/Scripts/python.exe"
+#fi
 
-popd  > /dev/null
+#popd  > /dev/null
 
-$PYTHON ${NPM_ROOT}/${PACKAGE}/hflow-viz-trace/main.py $@
+#$PYTHON ${NPM_ROOT}/${PACKAGE}/hflow-viz-trace/main.py $@
+
+python ${NPM_ROOT}/${PACKAGE}/hflow-viz-trace/main.py $@
